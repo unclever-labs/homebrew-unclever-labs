d7276a0957efe3a18ca62da4d5565b471416b259
Ryan Smith <rms1000watt@gmail.com>
2019-06-20 12:48:14 -0700

    updated

--- scidr.rb
+++ scidr.rb
@@ -1,9 +1,10 @@
 require "FileUtils"
 
+v = "0.0.1"
+
 class Scidr < Formula
   homepage "https://github.com/unclever-labs/scidr"
 
-  v = "0.0.1"
   url "https://github.com/unclever-labs/scidr/releases/download/#{v}/scidr-#{v}-darwin-amd64"
   version v
   sha256 "50808df5d1e2ea97638e5dc1532828883475ad6cbb6e44bdb7a8b5b370b79025"
