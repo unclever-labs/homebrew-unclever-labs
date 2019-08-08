f2734e0dc9f61f8b545dd08ce70e8d1993de7500
Ryan Smith <rms1000watt@gmail.com>
2019-08-08 09:44:10 -0700

    more general ruby

--- scidr.rb
+++ scidr.rb
@@ -1,17 +1,18 @@
 require "FileUtils"
 
 $v = "0.0.1"
+$p = "scidr"
 
 class Scidr < Formula
-  homepage "https://github.com/unclever-labs/scidr"
+  homepage "https://github.com/unclever-labs/#{$p}"
 
-  url "https://github.com/unclever-labs/scidr/releases/download/#{$v}/scidr-#{$v}-darwin-amd64"
+  url "https://github.com/unclever-labs/#{$p}/releases/download/#{$v}/#{$p}-#{$v}-darwin-amd64"
   version $v
   # shasum -a 256 < "bin/${PROJECT}-${VERSION}-${GOOS}-${GOARCH}" | cut -d ' ' -f1
   sha256 "50808df5d1e2ea97638e5dc1532828883475ad6cbb6e44bdb7a8b5b370b79025"
 
   def install
-    FileUtils.mv("scidr-#{$v}-darwin-amd64", "scidr")
-    bin.install "scidr"
+    FileUtils.mv("#{$p}-#{$v}-darwin-amd64", "#{$p}")
+    bin.install "#{$p}"
   end
 end
